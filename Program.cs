00c07bcb47b7a1f78a076603bb449b66f5923a2d
h-nkm <hiinakamu@gmail.com>
2021-06-08 19:50:24 +0900

    add

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int i = 0;
+
+            if (i == 0)
+            {
+                Console.WriteLine("Hello World!2");
+            }
+            else
+            {
+                Console.WriteLine("Hello World!2");
+            }
+
+        }
+    }
+}
